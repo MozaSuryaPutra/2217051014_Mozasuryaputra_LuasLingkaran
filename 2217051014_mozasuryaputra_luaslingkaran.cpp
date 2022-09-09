4a31e1992047fc7020d2018f24076cee49f65fd0
Moza Surya Putra <112565874+MozaSuryaPutra@users.noreply.github.com>
2022-09-09 15:17:14 +0700

    Add files via upload

---
+++ 2217051014_mozasuryaputra_luaslingkaran.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	const float phi = 3.14 ;
+	float l,r;
+	cout<<"masukan jari jari lingkaran = ";
+	cin>>r;
+	l = phi * r * r;
+	cout<<" luas lingkaran dengan jari jari "<<r<<" cm "<<" adalah ";
+	cout<<l<<"cm2";
+	
+}
